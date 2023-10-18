e51324d0b7419661e90eb5b8eb902b1d61d47008
jyoti-85 <107577241+jyoti-85@users.noreply.github.com>
2023-10-18 13:48:55 -0400

    Update roi_calculator_pharma.py

--- roi_calculator_pharma.py
+++ roi_calculator_pharma.py
@@ -3,58 +3,38 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 
-
-# Function to generate sample data
-def generate_sample_data(quarters=5):
-    data = {
-        "Quarters": [],
-        "Submissions": [],
-        "Approvals": [],
-        "Competitor_Approvals": []
-    }
-    
-    for quarter in range(1, quarters+1):
-        year = (quarter-1)//4 + 1
-        qtr = (quarter-1)%4 + 1
-        
-        submissions = random.randint(100, 200) * quarter
-        approvals = random.randint(50, submissions)
-        competitor_approvals = random.randint(25, approvals)
-        
-        data["Quarters"].append(f"Y{year}Q{qtr}")
-        data["Submissions"].append(submissions)
-        data["Approvals"].append(approvals)
-        data["Competitor_Approvals"].append(competitor_approvals)
-    
-    return data
+# Hardcoded sample data
+sample_data = {
+    "Quarters": ["Y1Q1", "Y1Q2", "Y1Q3", "Y1Q4", "Y2Q1"],
+    "Submissions": [100, 200, 300, 400, 500],
+    "Approvals": [50, 100, 150, 200, 250],
+    "Competitor_Approvals": [100, 95, 90, 85, 80]
+}
 
 # Streamlit interface
-st.title('Prior Authorizations over Quarters')
+st.title('Prior Authorizations Analytics')
 
-# Button to generate new sample data
-if st.button('Generate Sample Data'):
-    sample_data = generate_sample_data()
+# Extracting data
+quarters = sample_data["Quarters"]
+submissions = sample_data["Submissions"]
+approvals = sample_data["Approvals"]
+competitor_approvals = sample_data["Competitor_Approvals"]
 
-    # Extracting data
-    quarters = sample_data["Quarters"]
-    submissions = sample_data["Submissions"]
-    approvals = sample_data["Approvals"]
-    competitor_approvals = sample_data["Competitor_Approvals"]
+# Creating the graph
+fig, ax = plt.subplots()
+ax.plot(quarters, submissions, label='Number of Submissions', marker='o')
+ax.plot(quarters, approvals, label='Number of Approvals', marker='o')
+ax.plot(quarters, competitor_approvals, label='Competitor Approvals', marker='o')
 
-    # Creating the graph
-    fig, ax = plt.subplots()
-    ax.plot(quarters, submissions, label='Number of Submissions', marker='o')
-    ax.plot(quarters, approvals, label='Number of Approvals', marker='o')
-    ax.plot(quarters, competitor_approvals, label='Competitor Approvals', marker='o')
+ax.set_xlabel('Quarters')
+ax.set_ylabel('# of Prior Authorizations')
+ax.set_title('Prior Authorizations over Quarters')
+ax.legend()
+ax.set_xticklabels(quarters, rotation=45)  # Rotate quarter labels for better visibility
 
-    ax.set_xlabel('Quarters')
-    ax.set_ylabel('# of Prior Authorizations')
-    ax.set_title('Prior Authorizations over Quarters')
-    ax.legend()
-    ax.set_xticklabels(quarters, rotation=45)  # Rotate quarter labels for better visibility
+# Displaying the graph in Streamlit
+st.pyplot(fig)
 
-    # Displaying the graph in Streamlit
-    st.pyplot(fig)
 
 
 # Hardcoded data for the Provider Analytics table
