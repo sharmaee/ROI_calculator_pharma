d1670779a065febc09e44e50afdfd79f02a4346a
jyoti-85 <107577241+jyoti-85@users.noreply.github.com>
2023-10-18 13:29:33 -0400

    Update roi_calculator_pharma.py

--- roi_calculator_pharma.py
+++ roi_calculator_pharma.py
@@ -3,20 +3,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 
+import streamlit as st
+import matplotlib.pyplot as plt
+import random
+
 # Function to generate sample data
-def generate_sample_data(years=10):
+def generate_sample_data(quarters=5):
     data = {
-        "Years": list(range(1, years+1)),
+        "Quarters": [],
         "Submissions": [],
         "Approvals": [],
         "Competitor_Approvals": []
     }
     
-    for year in data["Years"]:
-        submissions = random.randint(500, 1000) * year
-        approvals = random.randint(300, submissions)
-        competitor_approvals = random.randint(200, approvals)
+    for quarter in range(1, quarters+1):
+        year = (quarter-1)//4 + 1
+        qtr = (quarter-1)%4 + 1
         
+        submissions = random.randint(100, 200) * quarter
+        approvals = random.randint(50, submissions)
+        competitor_approvals = random.randint(25, approvals)
+        
+        data["Quarters"].append(f"Y{year}Q{qtr}")
         data["Submissions"].append(submissions)
         data["Approvals"].append(approvals)
         data["Competitor_Approvals"].append(competitor_approvals)
@@ -24,28 +32,29 @@ def generate_sample_data(years=10):
     return data
 
 # Streamlit interface
-st.title('Competition Analytics')
+st.title('Prior Authorizations over Quarters')
 
 # Button to generate new sample data
 if st.button('Generate Sample Data'):
     sample_data = generate_sample_data()
 
     # Extracting data
-    years = sample_data["Years"]
+    quarters = sample_data["Quarters"]
     submissions = sample_data["Submissions"]
     approvals = sample_data["Approvals"]
     competitor_approvals = sample_data["Competitor_Approvals"]
 
     # Creating the graph
     fig, ax = plt.subplots()
-    ax.plot(years, submissions, label='Number of Submissions', marker='o')
-    ax.plot(years, approvals, label='Number of Approvals', marker='o')
-    ax.plot(years, competitor_approvals, label='Competitor Approvals', marker='o')
+    ax.plot(quarters, submissions, label='Number of Submissions', marker='o')
+    ax.plot(quarters, approvals, label='Number of Approvals', marker='o')
+    ax.plot(quarters, competitor_approvals, label='Competitor Approvals', marker='o')
 
-    ax.set_xlabel('Years')
+    ax.set_xlabel('Quarters')
     ax.set_ylabel('# of Prior Authorizations')
-    ax.set_title('Prior Authorizations over Time')
+    ax.set_title('Prior Authorizations over Quarters')
     ax.legend()
+    ax.set_xticklabels(quarters, rotation=45)  # Rotate quarter labels for better visibility
 
     # Displaying the graph in Streamlit
     st.pyplot(fig)
